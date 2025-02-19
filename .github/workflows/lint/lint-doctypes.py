b4fbff46bd87c59bdb3e80c319d3865d35bc9301
Tab Atkins-Bittner <jackalmage@gmail.com>
2025-02-18 16:25:14 -0800

    fix the doctypes lint

--- .github/workflows/lint/lint-doctypes.py
+++ .github/workflows/lint/lint-doctypes.py
@@ -1,11 +1,7 @@
 from __future__ import annotations
 
-import os
-import sys
-import typing
-
 import kdl
 
-with io.open("boilerplate/doctypes.kdl", "r") as fh:
+with open("boilerplate/doctypes.kdl", "r") as fh:
 	text = fh.read()
 	kdl.parse(text)
