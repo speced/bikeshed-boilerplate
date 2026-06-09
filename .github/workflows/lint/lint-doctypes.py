0b44e5e9c017e229787a84b5b1390b9f149f4673
Tab Atkins-Bittner <jackalmage@gmail.com>
2026-06-09 15:35:45 -0700

    Update file location

--- .github/workflows/lint/lint-doctypes.py
+++ .github/workflows/lint/lint-doctypes.py
@@ -2,6 +2,6 @@ from __future__ import annotations
 
 import kdl
 
-with open("boilerplate/doctypes.kdl", "r") as fh:
+with open("boilerplate-v1/doctypes.kdl", "r") as fh:
 	text = fh.read()
 	kdl.parse(text)
